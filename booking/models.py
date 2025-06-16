5e9cc2c5fd5e147615ea1e8c32452ce2fa5b699f
DTT2411 <david.thomson2411@gmail.com>
2025-06-16 14:15:06 +0100

    Add a Reservation model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Reservation(models.Model):
+    guest = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reservations"
+    )
+    number_of_guests = models.IntegerField()
+    time = models.TimeField()
+    date = models.DateField()
+    duration = models.IntegerField()
+    special_reqs = models.TextField(max_length=500)
