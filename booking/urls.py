8aca68984483130d1ee1cdd6a9ab3b32abeedca6
DTT2411 <david.thomson2411@gmail.com>
2025-06-24 21:19:15 +0100

    Initialise booking form.

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # path('booking/', views.Booking.as_view(), name='booking'),
+    path('book/', views.create_reservation, name='create_reservation'),
     path('my_booking/', views.BookingList.as_view(), name='my_booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Reservation
+from .forms import ReservationForm
 # from django.http import HttpResponse
 
 
 # Create your views here.
 # def my_booking(request):
 #     return HttpResponse("Hello, this is the booking page!")
+
+
+def create_reservation(request):
+    form = ReservationForm()
+    return render(
+        request,
+        'booking/reservation_form.html',
+        {
+            "form": form,
+        },
+    )
+
+# def delete_reservation(request):
+#     return
+
+# def display_homepage(request):
+#     return
 
 class BookingList(generic.ListView):
     queryset = Reservation.objects.all().order_by('date', 'time')
