4654570b142301f881bf8deba4e969b72ae7145b
DTT2411 <david.thomson2411@gmail.com>
2025-06-27 15:40:23 +0100

    Update secret key in env.py and heroku.

--- the_otter_mill/settings.py
+++ the_otter_mill/settings.py
@@ -128,7 +128,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
