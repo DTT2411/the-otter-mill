1f3ee2021065e4b273298ae1bdf2830fcb9d085f
DTT2411 <david.thomson2411@gmail.com>
2025-06-27 15:11:08 +0100

    Add test cases to test whether submitting empty fields for booking results in invalid/valid form.

--- booking/test_forms.py
+++ booking/test_forms.py
@@ -5,6 +5,9 @@ from .forms import ReservationForm
 class TestReservationForm(TestCase):
 
     def test_form_is_valid(self):
+        """
+        Tests for all valid fields.
+        """
         reservation_form = ReservationForm({
             'number_of_guests': '3',
             'date': '2025-07-01',
@@ -15,6 +18,9 @@ class TestReservationForm(TestCase):
         self.assertTrue(reservation_form.is_valid(), msg="Form is not valid")
 
     def test_form_is_invalid(self):
+        """
+        Tests for all empty fields.
+        """
         reservation_form = ReservationForm({
             'number_of_guests': '',
             'date': '',
@@ -23,3 +29,73 @@ class TestReservationForm(TestCase):
             'special_reqs': '',
         })
         self.assertFalse(reservation_form.is_valid(), msg="Form is valid")
+
+    def test_number_of_guests_is_required(self):
+        """
+        Tests whether empty field for number_of_guests results in invalid form.
+        """
+        reservation_form = ReservationForm({
+            'number_of_guests': '',
+            'date': '2025-07-01',
+            'time': '20:30:00',
+            'duration': '3',
+            'special_reqs': 'None',
+        })
+        self.assertFalse(reservation_form.is_valid(),
+                         msg="Empty number_of_guests field is valid")
+
+    def test_date_is_required(self):
+        """
+        Tests whether empty field for date results in invalid form.
+        """
+        reservation_form = ReservationForm({
+            'number_of_guests': '3',
+            'date': '',
+            'time': '20:30:00',
+            'duration': '3',
+            'special_reqs': 'None',
+        })
+        self.assertFalse(reservation_form.is_valid(),
+                         msg="Empty date field is valid")
+
+    def test_time_is_required(self):
+        """
+        Tests whether empty field for time results in invalid form.
+        """
+        reservation_form = ReservationForm({
+            'number_of_guests': '3',
+            'date': '2025-07-01',
+            'time': '',
+            'duration': '3',
+            'special_reqs': 'None',
+        })
+        self.assertFalse(reservation_form.is_valid(),
+                         msg="Empty time field is valid")
+
+    def test_duration_is_required(self):
+        """
+        Tests whether empty field for duration results in invalid form.
+        """
+        reservation_form = ReservationForm({
+            'number_of_guests': '3',
+            'date': '2025-07-01',
+            'time': '20:30:00',
+            'duration': '',
+            'special_reqs': 'None',
+        })
+        self.assertFalse(reservation_form.is_valid(),
+                         msg="Empty number_of_guests field is valid")
+
+    def test_special_reqs_is_not_required(self):
+        """
+        Tests whether empty field for special_reqs results in valid form.
+        """
+        reservation_form = ReservationForm({
+            'number_of_guests': '3',
+            'date': '2025-07-01',
+            'time': '20:30:00',
+            'duration': '3',
+            'special_reqs': '',
+        })
+        self.assertTrue(reservation_form.is_valid(),
+                        msg="Empty number_of_guests field is not valid")
