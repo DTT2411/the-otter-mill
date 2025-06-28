402154849351b7ef6876d9dfc936b558da51d70d
DTT2411 <david.thomson2411@gmail.com>
2025-06-28 14:56:57 +0100

    Update serving of static files prior to deployment test.

--- staticfiles/js/booking.js
+++ staticfiles/js/booking.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Confirmation check for delete button(s)
     document.querySelectorAll('form.delete-reservation').forEach(function(form) {
         form.addEventListener('submit', function(event) {
             if (!confirm('Are you sure you want to delete this booking?')) {
